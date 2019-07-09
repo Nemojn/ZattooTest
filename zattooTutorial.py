b10231290eb3269073d40b8fff30f7a79b5950a7
nemojn <nemo@eliworks.com>
2019-07-09 17:21:24 -0400

    Increased error handling capabilities, added in last two API calls, and made app_tid a secret

--- zattooTutorial.py
+++ zattooTutorial.py
@@ -4,8 +4,20 @@ import uuid
 import json
 import socket
 import urllib3
+#importing secret app tid
+from SecretParams import *
 
-#setup uuid
+#status boolean initialization
+internetConnection = False
+
+#Non-applicable if there's no internet connection 
+helloSuccessful = False
+watchSuccessful = False
+disabledChannel = False
+endedSession = False
+
+
+#creates uuid
 deviceUUID = uuid.uuid4()
 uuid_str = deviceUUID.urn
 uuid_str = uuid_str[9:]
@@ -15,44 +27,83 @@ deviceUUID = uuid_str
 sess = requests.Session()
 
 #setting up parameters to send in query
-helloQuery = "?app_tid=7796241b-eb33-45f5-b677-76ff433b7663&uuid=" + deviceUUID + "&lang=en&format=json"
+helloQuery = "?app_tid=" + app_tid + "&uuid=" + deviceUUID + "&lang=en&format=json"
 
 print('Starting session...')
 
 #starts session
 try:
 	helloResponse = sess.post('https://sandbox.zattoo.com/zapi/session/hello'+helloQuery)
+	#converts response into JSON object
+	helloResponse = json.loads(helloResponse.text)
 #error handling for no internet connection
 except (socket.gaierror, urllib3.exceptions.NewConnectionError, urllib3.exceptions.MaxRetryError, requests.exceptions.ConnectionError):
+	internetnetConnection = False
 	print("Unable to connect to server. Check your internet connection")
+except ValueError:
+	internetConnection = True
+	print("Error: Response not in JSON format.")
 else:
-	#converts response to json object
-	helloResponse = json.loads(helloResponse.text)
-	
+	internetConnection = True
+
 	#makes the json object nice and easy to read
 	print(json.dumps(helloResponse, indent=4, sort_keys=True))
-	
-	#preparing watch query
-	watchQuery = "?cid=3sat&stream_type=hls"
-	
-	print('Successfully connected to server.')
-	print('Requesting stream url...')
-	
-	#requests stream url
-	watchResponse = sess.post('https://sandbox.zattoo.com/zapi/watch'+watchQuery)
-	
-	#converts watch response into json object
-	try:
-		watchResponse = json.loads(watchResponse.text)
-	except ValueError as e:
-		print(watchResponse.text)
+	if helloResponse['success'] is 'false':
+		try:
+			helloError = str(helloResponse['http_status'])
+		except KeyError:
+			helloError = "none"
 	else:
-		#formats json object into more convenient form
-		print(json.dumps(watchResponse, indent=4, sort_keys=True))
+		helloSuccess = True
+		print('Successfully connected to server.')
+		#preparing watch query
+		watchQuery = "?cid=3sat&stream_type=hls"
+		print('Requesting stream url...')
 		
-		if watchResponse['success'] is 'true':
-			#finds and prints stream url
-			print('Stream url: '+watchResponse['stream']['url'])
+		#requests stream url
+		try:
+			watchResponse = sess.post('https://sandbox.zattoo.com/zapi/watch'+watchQuery)
+			
+			#converts watch response into json object
+			watchResponse = json.loads(watchResponse.text)
+		except (socket.gaierror, urllib3.exceptions.NewConnectionError, urllib3.exceptions.MaxRetryError, requests.exceptions.ConnectionError):
+			internetConnection = False
+			print("Unable to connect to server. Check your internet connection.")
+		except ValueError:
+			print("Error: Response not in JSON format.")
 		else:
-			httpError = str(watchResponse['http_status'])
-			print("Unable to fetch watch URL. HTTP Error: "+httpError)
+			#formats json object into more convenient form
+			print(json.dumps(watchResponse, indent=4, sort_keys=True))
+			
+			if watchResponse['success'] is 'true':
+				#finds and prints stream url
+				watchSuccess = True
+				print('Stream url: '+watchResponse['stream']['url'])
+			else:
+				try:
+					watchError = str(watchResponse['http_status'])
+					print("Unable to fetch watch URL. HTTP Error: " + watchError)
+				except KeyError:
+					watchError = "none"
+		finally:
+			#stopping test channel
+			print('Stopping test channel...')
+			stopResponse = sess.get('https://sandbox.zattoo.com/zapi/stop')
+			
+			stopResponse = json.loads(stopResponse.text)
+			
+			print(json.dumps(watchResponse, indent=4, sort_keys=True))
+			
+			print('Test channel stopped.')
+
+			#stopping session
+			print('Stopping session...')
+			stopSessionResponse = sess.post('https://sandbox.zattoo.com/zapi/session/goodbye')
+			
+			stopSessionResponse = json.loads(stopSessionResponse.text)
+			print(json.dumps(stopSessionResponse, indent=4, sort_keys=True))
+			print('Session stopped.')
+finally:
+	#Status report printing
+	print("\nStatus:\n\nConnected to internet: " + str(internetConnection) + "\nHello call successful: " + str(helloSuccessful) + 
+	"\nWatch call successful: " + str(watchSuccessful) + "\nChannel disabled: " + str(disabledChannel) + "\nSession stopped: " + str(endedSession))
